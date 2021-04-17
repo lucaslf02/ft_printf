3a0675d186920f7cf56de5ed7e3febbd8dad71c5
Lucas Lemes <llemes-f@student.42sp.org.br>
2021-04-17 17:58:52 -0300

    Add main.c with basic tests implemented

---
+++ main.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llemes-f <llemes-f@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/16 21:43:15 by llemes-f          #+#    #+#             */
+/*   Updated: 2021/04/17 17:57:20 by llemes-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/ft_printf.h"
+#include "libft/libft.h"
+#include "stdio.h"
+
+int	main(void)
+{
+	char test_char = 'A';
+	char *test_string = "LUCAS";
+	int test_int = 42;
+	float test_float = 42.21;
+	long int test_long_int = 2147483647;
+	unsigned int test_usig_int = 42;
+
+	/*Apenas texto*/
+	printf("Apenas texto\n"); 
+	printf("Printf:\t\t\tApenas texto\n"); 
+	ft_printf("ft_printf: Apenas texto\n");
+	printf("\n");
+	/*Teste Char*/
+	printf("Teste Char\n");
+	printf("Printf:\t\t\t%c\n",test_char); 
+	ft_printf("ft_printf: %c\n",test_char);
+	printf("\n");
+	/*Teste String*/
+	printf("Teste String\n");
+	printf("Printf:\t\t\t%s\n",test_string); 
+	ft_printf("ft_printf: %s\n",test_string);
+	printf("\n");
+	/*Teste INT*/
+	printf("Teste INT\n");
+	printf("Usando d\n");
+	printf("Printf:\t\t\t%d\n",test_int); 
+	ft_printf("ft_printf: %d\n",test_int);
+	printf("Usando i\n");
+	printf("Printf:\t\t\t%i\n",test_int); 
+	ft_printf("ft_printf: %i\n",test_int);
+	printf("\n");
+	/*Teste Float*/
+	printf("Teste Float\n");
+	printf("Printf:\t\t\t%f\n",test_float); 
+	ft_printf("ft_printf: %f\n",test_float);
+	printf("\n");
+	/*Teste LONG INT*/
+	printf("Teste LONG INT\n");
+	printf("Usando ld\n");
+	printf("Printf:\t\t\t%ld\n",test_long_int); 
+	ft_printf("ft_printf: %ld\n",test_long_int);
+	printf("Usando li\n");
+	printf("Printf:\t\t\t%li\n",test_long_int); 
+	ft_printf("ft_printf: %li\n",test_long_int);
+	printf("\n");
+	/*Teste UNSIGNED INT*/
+	printf("Teste UNSIGNED INT\n");
+	printf("Usando ud\n");
+	printf("Printf:\t\t\t%ud\n",test_usig_int); 
+	ft_printf("ft_printf: %ud\n",test_usig_int);
+	printf("Usando ui\n");
+	printf("Printf:\t\t\t%ui\n",test_usig_int); 
+	ft_printf("ft_printf: %ui\n",test_usig_int);
+	printf("\n");
+	return (0);
+}
